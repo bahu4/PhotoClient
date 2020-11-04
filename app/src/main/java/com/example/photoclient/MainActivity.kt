7d20fa7cc5aa8740755ed8c02b0d403fc5c2b2f5
bahu4 <bahu4@mail.ru>
2020-11-05 01:12:50 +0300

    add menu item add SettingsActivity.kt

--- app/src/main/java/com/example/photoclient/MainActivity.kt
+++ app/src/main/java/com/example/photoclient/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.photoclient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
