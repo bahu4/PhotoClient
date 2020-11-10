e8677826331a32b022cc471a09d84c660a3a1bc5
bahu4 <bahu4@mail.ru>
2020-11-10 23:49:33 +0300

    add fragment

--- app/src/main/java/com/example/photoclient/MainActivity.kt
+++ app/src/main/java/com/example/photoclient/MainActivity.kt
@@ -1,29 +1,15 @@
 package com.example.photoclient
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
-            }
-        }
-        return super.onOptionsItemSelected(item)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainContainer, MainFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/photoclient/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.example.photoclient
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
